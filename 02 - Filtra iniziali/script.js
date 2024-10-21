f97bc787a4a2bc66c4b3ec27a4e34d339fd250b3
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-21 16:34:50 +0200

    es3 + es4

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,12 +5,21 @@ const searchedLetter = "A";
 let newArray = [];
 
 // Dichiara la funzione qui.
+/**
+ * returns an array of array of the words in the original array that start with the searched letter
+ * @param {Array} array array to search in
+ * @param {Array} searchedLetter letter to search for
+ * @returns {Array} array of the words in the original array that start with the searched letter
+ */
 const arrowFirstLetter = (array, searchedLetter) => {
+  // * dichiaro nuovo array
   const arrayFirstLetter = [];
+  // * per ogni elemento dell'array originale, se l'elemento comincia con la lettera cercata, lo aggiungo al nuovo array
   array.forEach((currentItem) => {
     if (currentItem.charAt(0) === searchedLetter)
       arrayFirstLetter.push(currentItem);
   });
+  // * ritorno il nuovo array
   return arrayFirstLetter;
 };
 // Invoca la funzione qui e stampa il risultato in console
--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,28 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
+const vowels = ["a", "e", "i", "o", "u"];
 
 // Dichiara la funzione qui.
-
+/**
+ * given a string and an array of chars to be searched returns the number of occurrencies of one of the items in whitelist inside of the word to search into
+ * @param {String} word word to search into
+ * @param {Array} whitelist char to be searched for
+ * @returns {Number} number of occurrencies of one of the items in whitelist inside of the word to search into
+ */
+function numberOfOccurrencies(word, whitelist) {
+  // * inizializzo un contatore a zero
+  let counter = 0;
+  // * per ogni carattere della parola (trasformo in array di caratteri per poter usare il foreach)
+  [...word].forEach((currentItem) => {
+    // * SE il carattere è compreso nella whitelist, aumento il contatore
+    if (whitelist.includes(currentItem)) counter++;
+  });
+  // * ritorno il contatore
+  return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(numberOfOccurrencies(word, vowels));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
