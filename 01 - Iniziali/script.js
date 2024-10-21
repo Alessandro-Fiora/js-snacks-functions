1f4be1869e8e8f2aabe466af0135e17cea70559b
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-21 15:43:54 +0200

    es1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,26 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+/**
+ * Given an array of strings, returns an array of the first letters of the elements of the original array
+ * @param {Array} originalArray the complete array
+ * @returns {Array} an array of the first letters of the elements of the original array
+ */
+function firstLetter(originalArray) {
+  // * dichiaro nuovo array vuoto
+  const fistLetterArray = [];
+  // * per ogni elemento dell'array originale pusho la prima lettera dell'elemento corrente nel nuovo array
+  originalArray.forEach((currentWord) =>
+    fistLetterArray.push(currentWord.charAt(0))
+  );
+  return fistLetterArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const newArray = firstLetter(names);
+console.log(newArray);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
